474cb7b0c939463d1e1bb51fcccef43d038c986b
edwardmuss <edwardmuss5@gmail.com>
2021-08-25 16:32:30 +0300

    tell me Betty will now be  proud

--- env_builtin.c
+++ env_builtin.c
@@ -1,18 +1,16 @@
 /*
- * File: env_builtins.c
+ * File: env_builtin.c
  */
 
 #include "shell.h"
 
 /**
  * builtin__env - handles the `env` command (displays environment variables)
- * @params: command arguments
  *
  * Return: command exit status
  */
 int builtin__env(void)
 {
-	/* char **args __attribute__((unused)) = params.args;*/
 	char **env = NULL;
 	uint env_i = 0;
 
